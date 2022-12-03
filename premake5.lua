15d7cc22f3d66101c5c0473afc5ec426f4888375
BigHug <bighug.yt@gmail.com>
2022-12-03 21:19:46 +0100

    Set up the repository to be a submodule

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "Crayon"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		
+		"src/**.h",
+		"src/**¨.cpp",
+
+		"include/**.h"
+	}
+
+	includeDirs {
+
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
